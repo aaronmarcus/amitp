d0cf0636c5b8c8d14e7f4385c1f5260bd9834aae
Aaron Marcus <ad.marcus7@gmail.com>
2021-01-10 17:58:25 +0000

    Update to correct template

--- amitp/main.cpp
+++ amitp/main.cpp
@@ -1,76 +1,16 @@
 #include <windows.h>
 
+#include "baseWindow.h"
 
-template <class DERIVED_TYPE>
-class BaseWindow
+//template for releasing items
+template <class T> void SafeRelease(T** ppT)
 {
-public:
-    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    if (*ppT)
     {
-        DERIVED_TYPE* pThis = NULL;
-
-        if (uMsg == WM_NCCREATE)
-        {
-            CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
-            pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
-            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
-
-            pThis->m_hwnd = hwnd;
-        }
-        else
-        {
-            pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
-        }
-        if (pThis)
-        {
-            return pThis->HandleMessage(uMsg, wParam, lParam);
-        }
-        else
-        {
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-        }
+        (*ppT)->Release();
+        *ppT = NULL;
     }
-
-    BaseWindow() : m_hwnd(NULL) { }
-
-    BOOL Create(
-        PCWSTR lpWindowName,
-        DWORD dwStyle,
-        DWORD dwExStyle = 0,
-        int x = CW_USEDEFAULT,
-        int y = CW_USEDEFAULT,
-        int nWidth = CW_USEDEFAULT,
-        int nHeight = CW_USEDEFAULT,
-        HWND hWndParent = 0,
-        HMENU hMenu = 0
-    )
-    {
-        WNDCLASS wc = { 0 };
-
-        wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
-        wc.hInstance = GetModuleHandle(NULL);
-        wc.lpszClassName = ClassName();
-
-        RegisterClass(&wc);
-
-        m_hwnd = CreateWindowEx(
-            dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
-            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
-        );
-
-        return (m_hwnd ? TRUE : FALSE);
-    }
-
-    HWND Window() const { return m_hwnd; }
-
-protected:
-
-    virtual PCWSTR  ClassName() const = 0;
-    virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
-
-    HWND m_hwnd;
-};
-
+}
 
 class MainWindow : public BaseWindow<MainWindow>
 {
@@ -80,11 +20,12 @@ public:
 };
 
 
+//windows entry point
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
     MainWindow win;
 
-    if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
+    if (!win.Create(L"Main Window", WS_OVERLAPPEDWINDOW))
     {
         return 0;
     }
@@ -103,7 +44,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     return 0;
 }
 
-
+//message handling
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
